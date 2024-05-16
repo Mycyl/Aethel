3383836f597f418d9622636e0271c09d5725d1c3
Michael Madrid <168844536+Mycyl@users.noreply.github.com>
2024-05-16 11:00:51 -0400

    Add files via upload

--- characters/player.py
+++ characters/player.py
@@ -8,18 +8,35 @@ class Player:
         self.image_size = self.image.get_size()
         self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
         self.delta = .25
+        self.jumping = False
 
     def move_direction(self, direction):
        if direction == "right":
            self.x += self.delta
-           self.y += self.delta
        elif direction == "left":
            self.x -= self.delta
-           self.y -= self.delta
        elif direction == "up":
            self.y -= self.delta
-           self.x += self.delta
        elif direction == "down":
-           self.x -= self.delta
            self.y += self.delta
        self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
+
+    def jump(self):
+        mass = 1
+        velocity = 0.2
+        keys = pygame.key.get_pressed()
+        if not(self.jumping):
+            if keys[pygame.K_SPACE]:
+                self.jumping = True
+        if self.jumping:
+            Force = (1 / 2) * mass * (velocity ** 2)
+            self.y -= Force
+            velocity -= 1
+            if velocity < 0:
+                mass *= -1
+            if velocity == -5:
+                self.jumping = False
+                mass = 1
+                velocity = 5
+        self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
+
--- main.py
+++ main.py
@@ -17,10 +17,11 @@ rolls = roll_num[0]
 # set up variables for the display
 size = (1200, 600)
 screen = pygame.display.set_mode(size)
-player = Player(0, 0)
+player = Player(200, 200)
 
 # The loop will carry on until the user exits the game (e.g. clicks the close button).
 run = True
+started = False
 
 # -------- Main Program Loop -----------
 while run:
@@ -32,22 +33,32 @@ while run:
     left_pressed = keys[pygame.K_a]
     down_pressed = keys[pygame.K_s]
     right_pressed = keys[pygame.K_d]
+    space_pressed = keys[pygame.K_SPACE]
 
-    if up_pressed:
-        player.move_direction("up")
-    if left_pressed:
-        player.move_direction("left")
-    if right_pressed:
-        player.move_direction("right")
-    if down_pressed:
-        player.move_direction("down")
+    if space_pressed and not(started):
+        started = True
+
+    if started:
+        if up_pressed:
+            player.move_direction("up")
+        if left_pressed:
+            player.move_direction("left")
+        if right_pressed:
+            player.move_direction("right")
+        if down_pressed:
+            player.move_direction("down")
 
     for event in pygame.event.get():  # User did something
         if event.type == pygame.QUIT:  # If user clicked close
             run = False
-
     screen.fill((0, 0, 0))
-    screen.blit(player.image, player.rect)
+    if started:
+        screen.blit(player.image, player.rect)
+        if space_pressed:
+            Player.jump(player)
+
+    print(player.x, player.y)
+
     pygame.display.update()
 
 # Once we have exited the main program loop we can stop the game engine:
